bf85466b9a784a39cc90ac49b5f8f7e0f1f6ecb9
ibrahimnadra <nadra.ibrahim@knoldus.com>
2023-04-09 11:57:20 +0530

    "Fixing the bug"

--- JavaProject/src/main/java/com/knoldus/Person.java
+++ JavaProject/src/main/java/com/knoldus/Person.java
@@ -1,5 +1,7 @@
 package com.knoldus;
-public class Person {
+
+public class Person{
+
  private String name;
  private Integer age;
  public Person() {
@@ -13,7 +15,6 @@ public class Person {
  public void setName(String name) {
   this.name = name;
  }
-
  public void setAge(Integer age) {
   this.age = age;
  }
@@ -28,7 +29,10 @@ public class Person {
 
  //Adding  a new method to the Person class that returns a boolean indicating whether
  //the person is an adult (age >= 18).
- public Boolean isAdult() {
+
+
+ //fixing a bug in the Person class,that is, renaming isAdult method to is Adult or not .
+ public Boolean isAdultOrNot() {
   if (this.age >= 18) return true;
   else return false;
  }
